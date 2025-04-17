3e635e8449532baf2ee9d5ec395f65521911e136
zcc <350018736@qq.com>
2025-04-17 17:49:02 +0800

    引入全局组件

---
+++ src/gComponents/g-flex/g-flex.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    dir?: 'row' | 'column'
+    justify?: 'initial' | 'flex-start' | 'flex-end' | 'center' | 'space-between' | 'space-around'
+    align?: 'initial' | 'flex-start' | 'flex-end' | 'center' | 'baseline' | 'stretch'
+    wrap?: boolean
+  }>(),
+  {
+    dir: 'row',
+    justify: 'initial',
+    align: 'initial',
+    wrap: false
+  }
+)
+</script>
+
+<template>
+  <view
+    class="flex"
+    :style="{
+      'flex-direction': dir,
+      'justify-content': justify,
+      'align-items': align
+    }"
+  >
+    <slot></slot>
+  </view>
+</template>
+
+<style lang="scss" scoped></style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,39 +1,13 @@
 <template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png" />
-    <wd-button>主要按钮</wd-button>
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
-    </view>
-  </view>
+  <g-flex><text>123</text><view>{{ title }}</view></g-flex>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
+
 const title = ref('Hello')
 </script>
 
-<style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.logo {
-  width: 200rpx;
-  height: 200rpx;
-  margin: 200rpx auto 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
-}
+<style lang="scss" scoped>
 </style>
+
