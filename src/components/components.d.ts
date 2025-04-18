1e504735f676119b7d300ed70dc6c7b4f8a2bd23
zcc <350018736@qq.com>
2025-04-18 11:25:16 +0800

    代码缓存

--- src/components/components.d.ts
+++ src/components/components.d.ts
@@ -1,9 +1,9 @@
-import CCFlex from '@/components/cc-flex/cc-flex.vue'
-import CCLayout from '@/components/cc-layout/cc-layout.vue'
+import CFlex from '@/components/c-flex/c-flex.vue'
+import CLayout from '@/components/c-layout/c-layout.vue'
 
 declare module 'vue' {
   export interface GlobalComponents {
-    CCFlex: typeof CCFlex
-    CCLayout: typeof CCLayout
+    CFlex: typeof CFlex
+    CLayout: typeof CLayout
   }
 }
